a756c4fdfb352c70691cf9b7cbbf6b5a6f08419d
DokPlay <serrggeejjj@gmail.com>
2026-02-04 17:01:53 +0300

    Add pagination validation in AdminUserController

--- main-service/src/main/java/ru/practicum/main/user/controller/AdminUserController.java
+++ main-service/src/main/java/ru/practicum/main/user/controller/AdminUserController.java
@@ -1,9 +1,12 @@
 package ru.practicum.main.user.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +29,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/admin/users")
 @Slf4j
+@Validated
 public class AdminUserController {
 
     private final UserService userService;
@@ -41,8 +45,8 @@ public class AdminUserController {
     @ResponseStatus(HttpStatus.OK)
     public List<UserDto> getUsers(
             @RequestParam(required = false) List<Long> ids,
-            @RequestParam(defaultValue = "0") int from,
-            @RequestParam(defaultValue = "10") int size) {
+            @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+            @RequestParam(defaultValue = "10") @Positive int size) {
         log.info("GET /admin/users - Получение пользователей: ids={}, from={}, size={}", ids, from, size);
         return userService.getUsers(ids, from, size);
     }
