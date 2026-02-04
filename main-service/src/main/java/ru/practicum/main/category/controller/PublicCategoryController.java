812c1729ebae1a8f0305853e5b35df222f932406
DokPlay <serrggeejjj@gmail.com>
2026-02-04 17:00:56 +0300

    Add pagination validation in PublicCategoryController

--- main-service/src/main/java/ru/practicum/main/category/controller/PublicCategoryController.java
+++ main-service/src/main/java/ru/practicum/main/category/controller/PublicCategoryController.java
@@ -1,7 +1,14 @@
 package ru.practicum.main.category.controller;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.main.category.dto.CategoryDto;
 import ru.practicum.main.category.service.CategoryService;
 
@@ -10,13 +17,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/categories")
+@Validated
 public class PublicCategoryController {
 
     private final CategoryService service;
 
     @GetMapping
-    public List<CategoryDto> getAll(@RequestParam(defaultValue = "0") int from,
-                                    @RequestParam(defaultValue = "10") int size) {
+    public List<CategoryDto> getAll(@RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                    @RequestParam(defaultValue = "10") @Positive int size) {
         return service.getAll(from, size);
     }
 
